eb7c5256d1c6754877cb874fca67f133cccc0e91
shakil ahmed <shakiltha338@gmail.com>
2024-06-16 14:33:43 +0600

    createYourStore for all screen complete"

--- src/components/CreateYourStore.jsx
+++ src/components/CreateYourStore.jsx
@@ -1,7 +1,25 @@
+import screens from "../assets/Screens.png";
 const CreateYourStore = () => {
   return (
-    <div className="pt-[70rem] border-2">
-      <h2 className="text-3xl">Create Your Store Section</h2>
+    <div className="pt-[57rem] lg:pt-[40rem] xl:pt-[50rem] mx-2">
+      <section className="lg:flex lg:items-center lg:gap-4">
+        <section className="lg:flex-1">
+          <img src={screens} alt="" />
+        </section>
+        <section className="lg:flex-1">
+          <p className="manrope font-medium">Build your business</p>
+          <h4 className="manrope font-semibold text-2xl lg:text-3xl">
+            Create your Store
+          </h4>
+          <p className="manrope font-normal lg:text-lg">
+            Ecommerce has opened doors for anyone with a passion and a product
+            to turn their dreams into a reality. Whether you're a seasoned
+            entrepreneur or just starting, creating your online store is easier
+            than ever. Let's explore the steps to take your business idea and
+            turn it into a thriving online destination.
+          </p>
+        </section>
+      </section>
     </div>
   );
 };
